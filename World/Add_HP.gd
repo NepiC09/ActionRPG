b2a557726255f615ea0bc6d234ec7ec709c6d087
NepiC09 <akmalkayum1@gmail.com>
2022-04-18 00:17:00 +0300

    ЗвукиСердцаМоего

--- World/Add_HP.gd
+++ World/Add_HP.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-onready var soundPlayer = $AudioStreamPlayer2D
-
-const Sound = preload("res://Player/PlayerHurtSound.tscn")
+const Sound = preload("res://UI/HealthSound.tscn")
 
 func _on_Area2D_body_entered(body):
 	if(body.stats.health < body.stats.max_health):
 		body.AddHealth()
 		
 		var sound = Sound.instance()
-		sound.set_stream(AudioStreamPlayer2D)
-		
 		get_tree().current_scene.add_child(sound)
-		
 		queue_free()
