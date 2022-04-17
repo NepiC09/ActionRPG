d5f4c4ae07d59aa7870063b5bf12bd90500d7b0b
telvanchik <Ewantarakudo@yandex.ru>
2022-04-18 02:34:51 +0300

    СУПЕР ПУПЕР СКАЯ АНИМАЦИЯ КНОПКИ

--- Menu.gd
+++ Menu.gd
@@ -2,14 +2,16 @@ extends Control
 #func _on_Button___pressed():
 	#get_tree().change_scene("res://World.tscn")
 
+	
 func load_game():
 	FS.load_game()
 
 #Кнопка Продолжить игру 
 func _on_TextureButton_pressed():
 	#FS.load_game()
-	get_tree().change_scene("res://World.tscn")
-	FS.load_game()
+	#get_tree().change_scene("res://World.tscn")
+	#FS.load_game()
+	pass
 
 #Выход из игры из главного меню
 func _on_Quit_pressed():
---
+++ Кнопка Продолжить игру.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+onready var Fade_In_Object = get_parent().get_node("Fade_In")
+onready var AnimationPlayerObject = get_parent().get_node("Fade_In/AnimationPlayer")
+
+func _ready():
+	Fade_In_Object.visible = false
+
+
+
+func _on____pressed():
+	Fade_In_Object.visible = true
+	AnimationPlayerObject.play("Fade_In")
+	yield(get_tree().create_timer(0.5), "timeout")
+	get_tree().change_scene("res://World.tscn")
+	FS.load_game()
