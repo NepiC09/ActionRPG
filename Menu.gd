5249d67e8a2130354ed767ec9361f59830b68805
NepiC09 <akmalkayum1@gmail.com>
2022-04-17 22:46:07 +0300

    Revert "й2йййй"
    This reverts commit 74ec03e4e5a019e6a4d74a86a96575e0b0839b02, reversing
    changes made to 5d7f118a31e9509587e47ca28a5c7abdbc2e7320.

--- Menu.gd
+++ Menu.gd
@@ -6,13 +6,8 @@ func load_game():
 	#FS.load_data()
 	#print(data)
 	pass
-<<<<<<< HEAD
 
 #Кнопка Продолжить игру 
-=======
-	
-	#продолжить
->>>>>>> 0ed6456e467b2429e23b6fc5ebc3d3a29cb2bdb8
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://World.tscn")
 	#load_game()
@@ -21,9 +16,3 @@ func _on_TextureButton_pressed():
 #Выход из игры из главного меню
 func _on_Quit_pressed():
 	get_tree().quit()
-
-#начать новую игру
-func _on_____pressed():
-	get_tree().change_scene("res://World.tscn")
-	PlayerStats.set_default()
-	pass # Replace with function body.
