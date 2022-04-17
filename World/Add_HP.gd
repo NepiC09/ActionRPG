35bae97480cc4a9bfd03dc6e284c25829832aafb
NepiC09 <akmalkayum1@gmail.com>
2022-04-17 20:21:25 +0300

    Что-то важное

--- World/Add_HP.gd
+++ World/Add_HP.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 
 func _on_Area2D_body_entered(body):
-	body.AddHealth()
-	queue_free()
+	if(body.stats.health < body.stats.max_health):
+		body.AddHealth()
+		queue_free()
+	else:
+		pass
